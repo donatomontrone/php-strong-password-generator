152280738653bb419a3ac62dda94e8f40b66ec87
donatomontrone <donato.montrone@outlook.com>
2023-01-26 14:43:09 +0100

    moved the logic to the functions.php file

--- index.php
+++ index.php
@@ -1,17 +1,12 @@
 <!-- Descrizione
 Dobbiamo creare una pagina che permetta ai nostri utenti di utilizzare il nostro generatore di password (abbastanza) sicure. L’esercizio è suddiviso in varie milestone ed è molto importante svilupparle in modo ordinato.
-Milestone 1
+Milestone 1 - DONE
 Creare un form che invii in GET la lunghezza della password.
 Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
 Scriviamo tutto (logica e layout) in un unico file index.php
 Milestone 2
-Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php che includeremo poi nella pagina principale --> <?php
-// Creo delle variabili che contengono tutti i caratteri possibili che la password randomica deve contenere.
-$alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHILJKMNOPQRSTUVWXYZ';
-$numbers = '0123456789';
-$symbols = '!£>$%&/()=&#167;@#.,-';
-
-?>
+Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php che includeremo poi nella pagina principale -->
+<!--  -->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -24,25 +19,9 @@ $symbols = '!£>$%&/()=&#167;@#.,-';
 
 <body>
     <main>
-        <div class="result">
-            <p> <?php
-                // Se l'utente ha inserito il numero prendo la lugnhezza data dall'input dell'utente.
-                if (isset($_GET['passwordLength'])) {
-                    $password_length = $_GET['passwordLength'];
-                    //unisco tutti i caratteri in un unica variabile 
-                    $allCharacters = $alphabet . $numbers . $symbols;
-                    //uso substr per generare una password dai caratteri partendo dal carattere 0, lunga quando specificato dall'utente. Attraverso str_shuffle la mischio.
-                    $password = substr(str_shuffle($allCharacters), 0, $password_length);
-                    echo $password;
-                    //Se l'utente non ha inserito la lunghezza della password genero un messaggio.
-                } else {
-                    echo '<p>';
-                    echo 'Inserisci la lughezza della tua password per generare la password';
-                    echo '</p>';
-                }
-
-                ?> </p>
-        </div>
+        <div class="result"> <?php
+            include_once __DIR__ . '/partials/functions.php';
+            ?> </div>
         <!-- Creo un form per effettuare la get sulla lugnhezza della password -->
         <form method="GET" action="./index.php">
             <label for="password">Inserisci la lunghezza della password</label>
---
+++ partials/functions.php
@@ -0,0 +1,22 @@
+<?php
+// Creo delle variabili che contengono tutti i caratteri possibili che la password randomica deve contenere.
+$alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHILJKMNOPQRSTUVWXYZ';
+$numbers = '0123456789';
+$symbols = '!<|\>$%&/()=&@#.,-';
+
+// Se l'utente ha inserito il numero prendo la lugnhezza data dall'input dell'utente.
+if (isset($_GET['passwordLength'])) {
+    $password_length = $_GET['passwordLength'];
+    //unisco tutti i caratteri in un unica variabile 
+    $allCharacters = $alphabet . $numbers . $symbols;
+    //uso substr per generare una password dai caratteri partendo dal carattere 0, lunga quando specificato dall'utente. Attraverso str_shuffle la mischio.
+    $password = substr(str_shuffle($allCharacters), 0, $password_length);
+    echo '<p>';
+    echo $password;
+    echo '</p>';
+    //Se l'utente non ha inserito la lunghezza della password genero un messaggio.
+} else {
+    echo '<p>';
+    echo 'Inserisci la lughezza della tua password per generare la password';
+    echo '</p>';
+}
