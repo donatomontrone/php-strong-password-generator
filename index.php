b579e105bc2c1d19b6f32316f0d6bfe594f1a1cf
donatomontrone <donato.montrone@outlook.com>
2023-01-26 15:06:52 +0100

    add function to generate password in functions.php

--- index.php
+++ index.php
@@ -4,9 +4,18 @@ Milestone 1 - DONE
 Creare un form che invii in GET la lunghezza della password.
 Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
 Scriviamo tutto (logica e layout) in un unico file index.php
-Milestone 2
+Milestone 2 DONE
 Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php che includeremo poi nella pagina principale -->
-<!--  -->
+<!-- Milestone 3 (BONUS)
+Invece di visualizzare la password nella index, effettuare un redirect ad una pagina dedicata che tramite $_SESSION recupererà la password da mostrare all’utente.
+Milestone 4 (BONUS)
+Gestire ulteriori parametri per la password: quali caratteri usare fra numeri, lettere e simboli.
+Possono essere scelti singolarmente (es. solo numeri) oppure possono essere combinati fra loro (es. numeri e simboli, oppure tutti e tre insieme).
+Dare all’utente anche la possibilità di permettere o meno la ripetizione di caratteri uguali.
+--> <?php
+    include_once __DIR__ . '/partials/functions.php';
+
+    ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -19,9 +28,11 @@ Verificato il corretto funzionamento del nostro codice, spostiamo la logica in u
 
 <body>
     <main>
-        <div class="result"> <?php
-            include_once __DIR__ . '/partials/functions.php';
-            ?> </div>
+        <div class="result">
+            <p> <?php
+                echo passwordGenerator($_GET['passwordLength']);
+                ?> </p>
+        </div>
         <!-- Creo un form per effettuare la get sulla lugnhezza della password -->
         <form method="GET" action="./index.php">
             <label for="password">Inserisci la lunghezza della password</label>
--- partials/functions.php
+++ partials/functions.php
@@ -1,22 +1,42 @@
 <?php
-// Creo delle variabili che contengono tutti i caratteri possibili che la password randomica deve contenere.
+/* // Creo delle variabili che contengono tutti i caratteri possibili che la password randomica deve contenere.
 $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHILJKMNOPQRSTUVWXYZ';
 $numbers = '0123456789';
 $symbols = '!<|\>$%&/()=&@#.,-';
 
-// Se l'utente ha inserito il numero prendo la lugnhezza data dall'input dell'utente.
+! Se l'utente ha inserito il numero prendo la lugnhezza data dall'input dell'utente.
 if (isset($_GET['passwordLength'])) {
     $password_length = $_GET['passwordLength'];
-    //unisco tutti i caratteri in un unica variabile 
+    !unisco tutti i caratteri in un unica variabile 
     $allCharacters = $alphabet . $numbers . $symbols;
-    //uso substr per generare una password dai caratteri partendo dal carattere 0, lunga quando specificato dall'utente. Attraverso str_shuffle la mischio.
+    !uso substr per generare una password dai caratteri partendo dal carattere 0, lunga quando specificato dall'utente. Attraverso str_shuffle la mischio.
     $password = substr(str_shuffle($allCharacters), 0, $password_length);
     echo '<p>';
     echo $password;
     echo '</p>';
-    //Se l'utente non ha inserito la lunghezza della password genero un messaggio.
+    !Se l'utente non ha inserito la lunghezza della password genero un messaggio.
 } else {
     echo '<p>';
     echo 'Inserisci la lughezza della tua password per generare la password';
     echo '</p>';
+} */
+
+
+//Creo una funzione che faccia il tutto
+
+function passwordGenerator($passLength)
+{
+    $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHILJKMNOPQRSTUVWXYZ';
+    $numbers = '0123456789';
+    $symbols = '!<|\>$%&/()=&@#.,-';
+    $password = '';
+
+    if (isset($passLength)) {
+        $allCharacters = $alphabet . $numbers . $symbols;
+        $password = substr(str_shuffle($allCharacters), 0, $passLength);
+    } else {
+        $password = 'Inserisci la lughezza della tua password per generare la password';
+    }
+
+    return $password;
 }
